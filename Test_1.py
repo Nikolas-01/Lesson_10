4a2f6149124ce5b1fe71667ed390916de17f8b49
ПК <89068414983@bk.ru>
2020-04-27 19:54:59 +0500

    1

---
+++ Test_1.py
@@ -0,0 +1,33 @@
+import pytest
+import module
+
+def test_deck():
+    deck = module.Deck()
+    assert len(deck._cards) == 36
+
+def test_suits():
+    deck = module.Deck()
+    assert len(deck.suits) == 4
+
+def test_list_card():
+	deck = module.Deck()
+	deck.mix_cards()
+	deck.list_card()
+	assert len(deck.list_num_card) == 36
+
+def test_player_hand():
+    deck = module.Deck()
+    deck.mix_cards()
+    deck.list_card()
+    deck.player_card()
+    deck.player_hand()
+    assert len(deck.cards_pl) == 6
+
+def test_computer_hand():
+    deck = module.Deck()
+    deck.mix_cards()
+    deck.list_card()
+    deck.player_card()
+    deck.player_hand()
+    deck.computer_hand()
+    assert len(deck.cards_comp) == 6
